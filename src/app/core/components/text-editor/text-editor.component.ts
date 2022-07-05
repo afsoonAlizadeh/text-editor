6e31a3b7f3a4c7d77106ec011ab80c44f5b5a295
Gunhild <afsoon.alizadeh1991@gmail.com>
2022-07-05 14:07:28 +0430

    create component

---
+++ src/app/core/components/text-editor/text-editor.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-text-editor',
+  templateUrl: './text-editor.component.html',
+  styleUrls: ['./text-editor.component.sass'],
+})
+export class TextEditorComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+  writeNewLine() {}
+}
---
+++ src/app/core/core.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { TextEditorComponent } from './components/text-editor/text-editor.component';
+@NgModule({
+  declarations: [TextEditorComponent],
+  imports: [],
+  exports: [],
+})
+export class CoreModule {}
