7d9b48888f78dd4fe2027fd04b79a11efbaa637b
Gunhild <afsoon.alizadeh1991@gmail.com>
2022-07-06 11:49:36 +0430

    add dynamic tag p when key press enter

--- src/app/core/components/text-editor/text-editor.component.ts
+++ src/app/core/components/text-editor/text-editor.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  HostListener,
+  OnInit,
+  Renderer2,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'app-text-editor',
@@ -6,8 +13,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./text-editor.component.sass'],
 })
 export class TextEditorComponent implements OnInit {
-  constructor() {}
+  @ViewChild('divMessages', { read: ElementRef })
+  private divMessages!: ElementRef;
+
+  text: string = '';
+  constructor(private renderer: Renderer2) {}
 
   ngOnInit(): void {}
-  writeNewLine() {}
+  @HostListener('window:keydown.enter', ['$event.target.innerHTML'])
+  onKeyPressHandler() {
+    // debugger;
+    let p = this.renderer.createElement('p');
+    // this.renderer.appendChild(p, event.target.innerHTML);
+    this.renderer.appendChild(this.divMessages.nativeElement, p);
+  }
 }
